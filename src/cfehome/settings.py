6db835f5e1e637566595f466646c932aa5ef673b
Mokyra18 <rizkyramadhani181102@gmail.com>
2024-07-16 12:40:08 +0700

    first commit

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # my-apps
+    "commando",
     "visits",
 ]
 
@@ -161,7 +162,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
-# STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
 # source(s) for python manage.py collectstatic 
